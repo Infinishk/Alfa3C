7f0caf2f1450c7479e54da6496aff6d0daa7f9e5
Jesus0204 <a01705442@tec.mx>
2024-09-11 16:54:48 -0600

    chore: organizar arquitectura

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Para usar express en vez de http
 const express = require('express');
 
-// Inicia la app usuando a express
+// Inicia la app usando express
 const app = express();
 
 // Configuramos a EJS como motor de templates con express
@@ -10,7 +10,7 @@ app.set('views', 'views');
 
 const path = require('path');
 
-// Para que se puede usar cookie parser de forma mas facil
+// Para que se pueda usar cookie parser de forma más fácil
 const cookieParser = require('cookie-parser');
 app.use(cookieParser('Un secreto'));
 
@@ -19,62 +19,79 @@ const session = require('express-session');
 
 app.use(session({
     secret: 's&xYnn9oVRuo3*0@sBA&SedkdMGoM!&e%kASzFfZ6537MqWruvYe27X=7hQUdktRRxYQHDjWtW7veznF',
-    resave: false, //La sesión no se guardará en cada petición, sino sólo se guardará si algo cambió 
-    saveUninitialized: false, //Asegura que no se guarde una sesión para una petición que no lo necesita
+    resave: false, // La sesión no se guardará en cada petición, sino sólo si algo cambió
+    saveUninitialized: false, // No guarda una sesión si no es necesario
+    rolling: true, // Renueva la sesión en cada interacción del usuario
     cookie: {
         maxAge: 900000 // Tiempo en milisegundos para que expire la sesión
     }
 }));
 
-// La aplicacion va a tener acceso a todo lo que esta en public
+// La aplicación va a tener acceso a todo lo que está en 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Manipular facil los datos de las peticiones
+// Para manipular fácilmente los datos de las peticiones
 const bodyParser = require('body-parser');
 
-// Configura bodyparser
+// Configura body-parser
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+app.use(bodyParser.json()); // Para manejar peticiones JSON
 
+// Rutas
+const rutasConfiguracion = require('./routes/configuracion.routes');
+app.use('/configuracion', checkSession, rutasConfiguracion);
+
+const rutasContrato = require('./routes/contrato.routes');
+app.use('/contrato', checkSession, rutasContrato);
+
+const rutasEstadoCuenta = require('./routes/estadoCuenta.routes');
+app.use('/estadoCuenta', checkSession, rutasEstadoCuenta);
+
+const rutasInquilino = require('./routes/inquilino.routes');
+app.use('/inquilino', checkSession, rutasInquilino);
+
+const rutasPagos = require('./routes/pagos.routes');
+app.use('/pagos', checkSession, rutasPagos);
+
+const rutasSession = require('./routes/session.routes');
+app.use('/auth', rutasSession);
+
+const rutasUsuarios = require('./routes/usuarios.routes');
+app.use('/usuarios', rutasUsuarios);
+
+// Middleware para manejar archivos con multer
 const multer = require('multer');
 const upload = multer(); // Utiliza multer sin configuración de almacenamiento
+app.use(upload.single('archivo')); // Para manejar un archivo enviado desde un formulario
 
-app.use(upload.single('archivo')); // Utiliza la configuración de multer sin almacenamiento
-// Para proteger del Cross-Site Request Forgery
+// Protección CSRF
 const csrf = require('csurf');
 const csrfProtection = csrf();
+app.use(csrfProtection); // Después de inicializar la sesión
 
-//...Y después del código para inicializar la sesión... 
-app.use(csrfProtection);
-
+// Seguridad con helmet
 const helmet = require('helmet');
-
 app.use(helmet({
     contentSecurityPolicy: {
         directives: {
             "script-src": ["'self'", 'code.jquery.com', 'ajax.googleapis.com', 'cdn.jsdelivr.net', 'cdnjs.cloudflare.com'],
-            "script-src-attr": ["'unsafe-inline'"], 
+            "script-src-attr": ["'unsafe-inline'"],
             "connect-src": ["'self'", 'sandboxpo.mit.com.mx'],
             "frame-src": ['*']
-        },
-    },
+        }
+    }
 }));
 
+// Compresión para mejorar el rendimiento
 const compression = require('compression');
-
 app.use(compression());
-
-// Para utilizar chartist para la generación de gráficos
-const chartist = require('chartist');
-
-app.use(bodyParser.json());
 
 // Middleware para verificar si la sesión está activa
 function checkSession(req, res, next) {
     if (!req.session.username) {
-        // Redirigir al login si no hay una sesión activa
-        return res.redirect('/auth/login');
+        return res.redirect('/auth/login'); // Redirigir al login si no hay una sesión activa
     }
     next();
 }
@@ -82,13 +99,12 @@ function checkSession(req, res, next) {
 // Middleware para manejar errores de CSRF
 app.use((err, req, res, next) => {
     if (err.code === 'EBADCSRFTOKEN') {
-        // Redirigir al login cuando se detecte un token CSRF inválido
-        return res.redirect('/auth/login');
+        return res.redirect('/auth/login'); // Redirigir al login si se detecta un token CSRF inválido
     }
     next(err);
 });
 
-// Agregar funcion para iterar la lista del ejs, y que el codigo se vea limpio
+// Función local para EJS, para iterar permisos
 app.locals.contienePermiso = (permisos, casoUso) => {
     const contains = !!permisos.find(caso => {
         return caso.funcion === casoUso;
@@ -96,10 +112,11 @@ app.locals.contienePermiso = (permisos, casoUso) => {
     return contains;
 };
 
+// Ruta para la página principal
 const home_root = require('./util/home');
 app.get('/', home_root);
 
-//Para error 404
+// Para manejar errores 404
 app.use((request, response, next) => {
     response.status(404);
     response.render('404', {
@@ -109,5 +126,5 @@ app.use((request, response, next) => {
     });
 });
 
-// Para que el servidor este activo
+// Iniciar el servidor
 app.listen(process.env.PORT || 5000);
