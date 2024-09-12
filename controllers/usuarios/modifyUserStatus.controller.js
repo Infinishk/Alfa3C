ffc8a5721d456d7f555eb69088dcd60200662ffe
Jesus0204 <a01705442@tec.mx>
2024-09-12 12:48:33 -0600

    fix: corregir parametros de llamada a BD

--- controllers/usuarios/modifyUserStatus.controller.js
+++ controllers/usuarios/modifyUserStatus.controller.js
@@ -1,16 +1,20 @@
 const Usuario = require('../../models/usuario.model');
 
 exports.postModifyUserStatus = (request, response, next) => {
-    const userID = request.body.userID;
     const status = request.body.status;
+    const userID = request.body.userID;
 
-    Usuario.modifyUserStatus(userID, status)
+    Usuario.modifyUserStatus(status, userID)
     .then(([answer, fieldData]) => {
-        response.status(200).json('El usuario ha sido modificado con exito');
+        response.status(200).json({
+            mensaje: 'El usuario ha sido modificado con exito'
+        });
     })
     .catch((error) => {
-        response.status(500).json('Hubo un error al modificar el usuario');
-        console.log(error);
+        response.status(500).json({
+            mensaje: 'Hubo un error al modificar el usuario', 
+            error: error
+        });
     });
 
 };
