44e9613b9f818aa11ec28bb0211c83dce059566b
Jesús Cedillo <65917649+Jesus0204@users.noreply.github.com>
2024-09-12 16:15:30 -0600

    chore: update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,11 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 
 export default [
-  // Primero aplicas las configuraciones recomendadas por Prettier
   pluginJs.configs.recommended,
 
   // Luego aplicas tus reglas personalizadas
   {
+    ignores: ['public/js/**/*.js'],
     languageOptions: {
       globals: globals.node,
     },
@@ -23,6 +23,30 @@ export default [
       'no-console': ['warn', {
         allow: ['warn', 'error']
       }]
+    },
+  },
+  {
+    files: ['public/js/**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        $: 'readonly', // Si utilizas jQuery
+        moment: 'readonly', // Moment.js
+      },
+    },
+    rules: {
+      'no-console': ['warn', {
+        allow: ['warn', 'error']
+      }], 
+      'no-unused-expressions': 'error',
+      'no-extra-semi': 'error', 
+      'no-alert': 'warn',
+      'camelcase': ['error', {
+        properties: 'always'
+      }], 
+      'strict': ['error', 'function'],
+      'no-script-url': 'error',
+      'no-implied-eval': 'error',
     },
   },
 ];
