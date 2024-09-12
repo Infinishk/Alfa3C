989a2d6d28ae646e39c2b685778c2f0069ac2686
Jesus0204 <a01705442@tec.mx>
2024-09-12 16:22:32 -0600

    chore: Arreglar eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,11 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 
 export default [
-  // Primero aplicas las configuraciones recomendadas por Prettier
   pluginJs.configs.recommended,
 
   // Luego aplicas tus reglas personalizadas
   {
+    ignores: ['public/js/**/*.js'],
     languageOptions: {
       globals: globals.node,
     },
@@ -17,12 +17,36 @@ export default [
         'vars': 'all',
         'args': 'none'
       }],
-      curly: ['error', 'all'], 
-      'no-var': 'error', 
-      'prefer-const': 'error', 
+      curly: ['error', 'all'],
+      'no-var': 'error',
+      'prefer-const': 'error',
       'no-console': ['warn', {
         allow: ['warn', 'error']
       }]
+    },
+  },
+  {
+    files: ['public/js/**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        $: 'readonly', // Si utilizas jQuery
+        moment: 'readonly', // Moment.js
+      },
+    },
+    rules: {
+      'no-console': ['warn', {
+        allow: ['warn', 'error']
+      }],
+      'no-unused-expressions': 'error',
+      'no-extra-semi': 'error',
+      'no-alert': 'warn',
+      'camelcase': ['error', {
+        properties: 'always'
+      }],
+      'strict': ['error', 'function'],
+      'no-script-url': 'error',
+      'no-implied-eval': 'error',
     },
   },
 ];
