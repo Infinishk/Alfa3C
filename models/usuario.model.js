f0c0b8a02ce7d4eee1d11724d8edb999400e74a4
Jesus0204 <a01705442@tec.mx>
2025-01-13 15:20:07 -0600

    fix: usar TypedSQL que es mas seguro en vez que raq queries

--- models/usuario.model.js
+++ models/usuario.model.js
@@ -1,5 +1,7 @@
 const prisma = require('../util/database'); 
 
+const { fetchActiveUsersDB, fetchInactiveUsersDB } = require('@prisma/client/sql');
+
 module.exports = class Usuario {
 
     static async modifyUserStatus(status, userID) {
@@ -14,17 +16,11 @@ module.exports = class Usuario {
     }
 
     static async fetchActiveUsers() {
-        return prisma.$queryRaw`
-        SELECT * FROM usuario AS U, posee AS P, cliente AS C WHERE U.Status = 1 
-        AND P.IDUsuario = U.IDUsuario AND U.IDUsuario = C.IDCliente AND P.IDRol = 'ROL02'
-        `;
+        return prisma.$queryRawTyped(fetchActiveUsersDB());
     }
 
     static async fetchInactiveUsers() {
-        return prisma.$queryRaw`
-        SELECT * FROM usuario AS U, posee AS P, cliente AS C WHERE U.Status = 0
-        AND P.IDUsuario = U.IDUsuario AND U.IDUsuario = C.IDCliente AND P.IDRol = 'ROL02'
-        `;
+        return prisma.$queryRawTyped(fetchInactiveUsersDB());
     }
 
     static async fetchAdmins() {
